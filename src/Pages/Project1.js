5d141605092a56ded03a201498cf2a6d72a54776
Jose <jose@trustyhacks.com>
2025-01-21 12:55:41 -0800

    Modified project tab

---
+++ src/Pages/Project1.js
@@ -0,0 +1,14 @@
+import projImgOne from "../Assets/device.png";
+
+function Project1() {
+  return (
+    <div className="text-white">
+      <h1 className="text-4xl">Smart Calendar and Reminder Device</h1>
+      <h2>March 8, 2024</h2>
+      <img src={projImgOne} className="pb-10" />
+      <p>Random text hello world diwjadiojawiod jiawjoi </p>
+    </div>
+  );
+}
+
+export default Project1;
